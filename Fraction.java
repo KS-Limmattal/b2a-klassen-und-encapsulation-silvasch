12d75526091579e17071a4f42bdf3c685de0abcb
Silvan Schmidt <mail@silvanschmidt.ch>
2024-11-06 08:25:18 +0100

    feat: simplify

--- Fraction.java
+++ Fraction.java
@@ -2,7 +2,11 @@ public class Fraction {
     static int numberOfFractions;
     private int numerator, denominator;
 
-    public Fraction(int numerator, int denominator) {
+    public Fraction(int numerator, int denominator) throws ArithmeticException {
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        
         this.numerator = numerator;
         this.denominator = denominator;
         numberOfFractions++;
@@ -14,6 +18,56 @@ public class Fraction {
 
     public Fraction() {
         this(0, 1);
+    }
+
+    public void add(Fraction f) {
+        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+        this.setDenominator(this.denominator * f.denominator);
+    }
+
+    public static Fraction add(Fraction f1, Fraction f2) {
+        return new Fraction(f1.numerator * f2.denominator + f1.denominator * f2.numerator, f1.denominator * f2.denominator);
+    }
+
+    public void sub(Fraction f) {
+        f.setNumerator(f.getNumerator() * -1);   
+        this.add(f);
+    }
+
+    public static Fraction sub(Fraction f1, Fraction f2) {
+        f2.setNumerator(f2.getNumerator() * -1);
+        return Fraction.add(f1, f2);
+    }
+   
+    public boolean equals(Fraction f) {
+        return this.numerator * f.denominator == this.denominator * f.numerator;
+    }
+
+    public void simplify() {
+        int gcd = gcd();
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+    }
+
+    public int gcd() {
+        int smallerNumber = 0;
+        if (this.numerator < this.denominator) {
+            smallerNumber = this.numerator;
+        } else {
+            smallerNumber = this.denominator;
+        }
+
+        for (int i = smallerNumber; i > 1; i--) {
+            if (this.numerator % i == 0 && this.denominator % i == 0) {
+                return i;
+            }
+        }
+
+        return 1;
+    }
+
+    public String toString() {
+        return String.format("%d/%d", this.numerator, this.denominator);
     }
 
     public int getNumerator() {
@@ -28,29 +82,12 @@ public class Fraction {
         this.numerator = numerator;
     }
 
-    public void setDenominator(int denominator) {
+    public void setDenominator(int denominator) throws ArithmeticException {
         if (denominator == 0) {
             throw new ArithmeticException("Division by zero");
         } else {
             this.denominator = denominator;
         }
-    }
-
-    public void add(Fraction f) {
-        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
-        this.setDenominator(this.denominator * f.denominator);
-    }
-
-    public static Fraction add(Fraction f, Fraction g) {
-        return new Fraction(f.numerator * g.denominator + f.denominator * g.numerator, f.denominator * g.denominator);
-    }
-    
-    public boolean equals(Fraction f) {
-        return this.numerator * f.denominator == this.denominator * f.numerator;
-    }
-
-    public String toString() {
-        return String.format("%d/%d", this.numerator, this.denominator);
     }
 
 }
--- FractionTester.java
+++ FractionTester.java
@@ -1,28 +1,53 @@
 public class FractionTester {
+    final double PI = 3.14159;
+    
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(3, 4);
-        Fraction f2 = new Fraction();
-        
-        // toString():
-        System.out.println(f1);
-        System.out.println(f2);
-
-        // Encapsulation, Getter und Setter:
-        assert f1.getNumerator() == 3;
-        assert f2.getDenominator() == 1;
-
-        // Copy-Konstruktor und equals():
-        Fraction f3 = new Fraction(f1);
-        assert f1.equals(f3);
-        
-        // Klassen- und Instanzenmethoden:
-        f3.add(f1);
-        Fraction f4 = Fraction.add(f1, f2);
-        assert f3.equals(new Fraction(3, 2));
-        assert f4.equals(new Fraction(3, 4));
-
-        // statische Variablen:
-        assert Fraction.numberOfFractions == 6;
+        toStringTest();
+        getSetTest();
+        copyTest();
+        addTest();
+        subTest();
+        simplifyTest();
     }
 
+    static void toStringTest() {
+        Fraction f = new Fraction(3, 4);
+        assert f.toString().equals("3/4");
+    }
+
+    static void getSetTest() {
+        Fraction f = new Fraction(3, 4);
+        f.setNumerator(1);
+        f.setDenominator(2);
+        assert f.equals(new Fraction(1, 2));
+    }
+
+    static void copyTest() {
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(f1);
+        assert f1.equals(f2);
+    }
+
+    static void addTest() {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(3, 4);
+        assert Fraction.add(f1, f2).equals(new Fraction(17, 12));
+    }
+
+    static void subTest() {
+        Fraction f1 = new Fraction(3, 4);
+        Fraction f2 = new Fraction(2, 4);
+        assert Fraction.sub(f1, f2).equals(new Fraction(1, 4));
+    }
+
+    static void simplifyTest() {
+        Fraction f1 = new Fraction(6, 9);
+        Fraction f2 = new Fraction(16, 8);
+
+        f1.simplify();
+        f2.simplify();
+
+        assert f1.equals(new Fraction(2, 3));
+        assert f2.equals(new Fraction(2, 1));
+    }
 }
